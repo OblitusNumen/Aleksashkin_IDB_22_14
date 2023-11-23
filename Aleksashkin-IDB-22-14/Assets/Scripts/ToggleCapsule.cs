2c910a0c3ed58eda54e98d38c38d499efb9c2ce8
snitch <o.aleksashkin@yandex.ru>
2023-11-23 23:52:56 +0300

    task-2

--- Aleksashkin-IDB-22-14/Assets/Scripts/ToggleCapsule.cs
+++ Aleksashkin-IDB-22-14/Assets/Scripts/ToggleCapsule.cs
@@ -1,29 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ToggleCapsule : MonoBehaviour
 {
     public GameObject capsule;
+    public TMP_Text text;
+    public int i = 100;
 
     public void toggleCapsule()
     {
         capsule.SetActive(!capsule.activeSelf);
         Debug.Log("Hello from ToggleCapsule.toggleCapsule");
-
+        
     }
 
     // Start is called before the first frame update
     void Start()
     {
         capsule = GameObject.Find("Capsule");
+        text = GameObject.Find("Text").GetComponent<TMP_Text>();
         Debug.Log("Hello from ToggleCapsule.Start");
-        
+        text.text = i + "";
     }
 
-    // Update is called once per frame
+    private void OnCollisionEnter(Collision other)
+    {
+        text.text = --i + "";
+    }
+
     void Update()
     {
-        
     }
 }
